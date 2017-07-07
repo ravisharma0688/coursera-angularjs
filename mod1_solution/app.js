0e1d4414703a8e59a51a7d92645563b924942a3f
Ravi <ravi@coursera-something.com>
2017-07-08 03:16:02 +0530

    Module 1 assignment solution

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,8 +1,7 @@
 (function () {
 'use strict';
-//var x="Hello";
-angular.module('LunchCheck', [])
 
+angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject =['$scope'];
@@ -17,43 +16,42 @@ $scope.displayMsg = function () {
   if($scope.items !== '') {
   var arrayOfStrings = $scope.items.split(',');
   var count=0;
-  for(var i =0; i<arrayOfStrings.length; i++){
-					if(arrayOfStrings[i].trim() !=='')
+    for(var i =0; i<arrayOfStrings.length; i++){
+			 if(arrayOfStrings[i].trim() !=='')
 						count++;
-          }
-          if(count > 0){
-        					if(count <=3){
-    $scope.msg='Enjoy!';
-  }
-
-  else if (count>3) {
-    $scope.msg='Too Much!';
-  }
-  $scope.fontStyle = {
-  						"color":"green"
-  					};
-            $scope.boxStyle = {
-            						"border-color":"green"
-            					};
-          }
-  else {
+    }
+    if(count > 0){
+      if(count <=3){
+         $scope.msg='Enjoy!';
+      }
+      else if (count>3) {
+         $scope.msg='Too Much!';
+      }
+         $scope.fontStyle = {
+  				"color":"green"
+  			 };
+         $scope.boxStyle = {
+          "border-color":"green"
+        };
+    }
+    else {
     enterDishes();
+    }
   }
-}
-			else {
-				enterDishes();
-			}
+	else {
+		enterDishes();
+	}
 }
 
 var enterDishes = function() {
 			$scope.msg = 'Please enter data first';
-				$scope.fontStyle = {
-					"color":"red"
-				};
-        $scope.boxStyle = {
-					"border-color":"red"
-				};
-		};
+			$scope.fontStyle = {
+				"color":"red"
+			};
+      $scope.boxStyle = {
+				"border-color":"red"
+			};
+	};
 }
 
 })();
