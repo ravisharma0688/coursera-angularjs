b3cc9a3f955a87ac5afd433bb759ff3bf196c3e1
Ravi <ravi@coursera-something.com>
2017-08-02 20:05:07 +0530

    Module 4 assignment solution

---
+++ mod4_solution/src/menuapp/categories.controller.js
@@ -0,0 +1,15 @@
+(function(){
+  'use strict'
+
+  angular.module("MenuApp")
+  .controller("CategoriesController",CategoriesController);
+
+  CategoriesController.$inject = ["categories"];
+  function CategoriesController(categories){
+//function CategoriesController(){
+    var ctrl = this;
+    ctrl.categories = categories;
+    //ctrl.name = "test";
+    //console.log(ctrl);
+  }
+})();
